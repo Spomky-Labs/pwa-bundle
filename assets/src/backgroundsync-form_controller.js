d68307a383645eaa8ed69c64a2657658c2852501
Florent Morselli <florent.morselli@spomky-labs.com>
2024-03-07 08:23:01 +0100

    FIX BackgroundSync SC

--- assets/src/backgroundsync-form_controller.js
+++ assets/src/backgroundsync-form_controller.js
@@ -5,39 +5,40 @@ import { Controller } from '@hotwired/stimulus';
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
     static values = {
-        params: { type: Array, default: {
+        params: { type: Object, default: {
             mode: 'cors',
             cache: 'no-cache',
             credentials: 'same-origin',
             redirect: 'follow',
             referrerPolicy: 'no-referrer'
         }},
-        headers: { type: Array, default: [] },
+        headers: { type: Object, default: {} },
         redirection: { type: String, default: null },
     };
 
     async onSubmit(event) {
         event.preventDefault();
-        const form = event.currentTarget;
+        const form = this.element;
         if (!form instanceof HTMLFormElement || !form.checkValidity()) {
+            console.error('Form is not valid', form, form.checkValidity());
             return;
         }
-        const url = form.action;
-        const encType = form.encType ?? 'application/x-www-form-urlencoded';
-        const method = form.method ?? 'POST';
-        const params = this.paramsValue;
-        params.headers = this.headersValue;
-        params.headers['Content-Type'] = encType;
+        console.error(form.method);
 
+        const url = form.action;
         try {
+            const params = this.paramsValue;
+            params.headers = this.headersValue;
+            params.headers['Content-Type'] = form.encType ?? 'application/x-www-form-urlencoded';
             params.body = new FormData(form);
+            params.method = form.method.toUpperCase();
             await fetch(url, params);
         } catch (error) {
+            console.error('Error while sending form', error);
             // No need to do anything here
         } finally {
-            if (this.redirectionValue) {
-                window.location.href = this.redirectionValue;
-            }
+            console.log('finally', this.redirectionValue);
+            window.location.href = this.redirectionValue || url;
         }
     }
 }
