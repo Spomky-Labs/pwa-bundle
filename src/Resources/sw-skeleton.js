a30813f4943612e45c6029d6e68a188f69c3f118
Florent Morselli <florent.morselli@spomky-labs.com>
2024-04-04 15:48:21 +0200

    Remove service worker file from src/Resources (#156)
    The 'sw-skeleton.js' file, previously located in src/Resources, has been deleted. This was a redundant file containing service worker rules, which are no longer necessary for our application's needs.

--- src/Resources/sw-skeleton.js
+++
@@ -1,6 +0,0 @@
-// *** Service Worker *** //
-/*
-    This is the service worker file. It will be populated with the rules you define in the
-    configuration file.
-    You can define here custom rules depending on your application needs.
- */
