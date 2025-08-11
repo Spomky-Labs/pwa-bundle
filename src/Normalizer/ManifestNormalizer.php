762c8bb28e10bd71b8b2839a0ca653622bdf8d68
Florent Morselli <florent.morselli@spomky-labs.com>
2025-08-11 17:55:23 +0200

    Add `ManifestNormalizer` for custom normalization of `Manifest` DTO

---
+++ src/Normalizer/ManifestNormalizer.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SpomkyLabs\PwaBundle\Normalizer;
+
+use SpomkyLabs\PwaBundle\Dto\Manifest;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use function array_key_exists;
+use function assert;
+
+final class ManifestNormalizer implements NormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    private const CTX_SKIP = 'manifest_pre_normalization_skip';
+
+    public function normalize(mixed $data, ?string $format = null, array $context = []): array
+    {
+        assert($data instanceof Manifest);
+        $context[self::CTX_SKIP] = true;
+
+        /** @var array<string,mixed> $normalized */
+        $normalized = $this->normalizer->normalize($data, $format, $context);
+
+        if (array_key_exists('lang', $normalized) && $normalized['lang'] !== null && $normalized['lang'] !== '') {
+            return $normalized;
+        }
+
+        $normalized['lang'] = $context['translatable_normalization_locale'] ?? null;
+
+        return $normalized;
+
+    }
+
+    public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
+    {
+        if (! $data instanceof Manifest) {
+            return false;
+        }
+
+        if (! empty($context[self::CTX_SKIP])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return array<class-string, bool>
+     */
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            Manifest::class => false,
+        ];
+    }
+}
