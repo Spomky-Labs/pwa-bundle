6865d052142c84c68382aa9bb68c7d4ff8f5205a
Florent Morselli <florent.morselli@spomky-labs.com>
2025-08-18 08:56:38 +0200

    Add install and phpqa_update tasks, update Docker volume configuration, and remove security advisories from composer.json

--- .ci-tools/rector.php
+++ .ci-tools/rector.php
@@ -3,42 +3,39 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
 use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonySetList;
+use Rector\Symfony\Symfony73\Rector\Class_\InvokableCommandInputAttributeRector;
 use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $config): void {
-    $config->sets([
-        SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_82,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        DoctrineSetList::DOCTRINE_CODE_QUALITY,
-        DoctrineSetList::DOCTRINE_ORM_214,
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ]);
-    $config->phpVersion(PhpVersion::PHP_82);
-    $config->paths(
-        [
-            __DIR__ . '/../src',
-            __DIR__ . '/../tests',
-            __DIR__ . '/../castor.php',
-            __DIR__ . '/ecs.php',
-            __DIR__ . '/rector.php',
-        ]
-    );
-    $config->rule(ExplicitNullableParamTypeRector::class);
-    $config->skip([
-        RemoveEmptyClassMethodRector::class => [__DIR__ . '/tests/Controller/'],
-    ]);
-    $config->parallel();
-    $config->importNames();
-    $config->importShortClasses();
-};
+$builder = RectorConfig::configure();
+if (file_exists('/tools/.composer/vendor-bin/phpunit/vendor/autoload.php')) {
+    $builder->withAutoloadPaths(['/tools/.composer/vendor-bin/phpunit/vendor/autoload.php']);
+}
+$builder->withSets([
+    SetList::DEAD_CODE,
+    LevelSetList::UP_TO_PHP_82,
+    DoctrineSetList::DOCTRINE_CODE_QUALITY,
+    DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+    PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+    PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
+]);
+$builder->withComposerBased(twig: true, doctrine: true, phpunit: true, symfony: true);
+$builder->withPhpVersion(PhpVersion::PHP_82);
+$builder->withPaths(
+    [
+        __DIR__ . '/../src',
+        __DIR__ . '/../tests',
+        __DIR__ . '/../castor.php',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/rector.php',
+    ]
+);
+$builder->withSkip([PreferPHPUnitThisCallRector::class, InvokableCommandInputAttributeRector::class]);
+$builder->withParallel();
+$builder->withImportNames();
+
+return $builder;
