675d83facfcf6cb1055f9e74c6b3a87f404221c2
Florent Morselli <florent.morselli@spomky-labs.com>
2025-08-24 21:21:28 +0200

    Remove unused BroadcastChannel methods from background sync form controller

--- assets/src/backgroundsync-form_controller.js
+++ assets/src/backgroundsync-form_controller.js
@@ -20,25 +20,7 @@ export default class extends AbstractController {
         redirection: { type: String, default: null },
         authenticating: { type: Boolean, default: false },
         keyIdIndex: { type: String, default: 'default' },
-        bcChannel: { type: String },
     };
-
-    bc = null;
-
-    connect = () => {
-        if (!this.channelValue) {
-            return
-        }
-        this.bc = new BroadcastChannel(this.channelValue);
-        this.bc.onmessage = (event) => this._onMessage(event.data);
-        this.bc.postMessage({ type: 'status-request' });
-    }
-
-    disconnect = () => {
-        if (this.bc !== null) {
-            this.bc.close();
-        }
-    }
 
     send = async (event) => {
         const form = this.element;
@@ -96,20 +78,6 @@ export default class extends AbstractController {
             if (redirectTo) {
                 window.location.assign(redirectTo);
             }
-        }
-    }
-
-    replay = () => {
-        if (this.bc === null) {
-            return;
-        }
-        this.bc.postMessage({ type: 'replay-request' });
-    }
-
-    _onMessage(event) {
-        const {remaining} = event;
-        if (Number.isInteger(remaining)) {
-            this.dispatchEvent('status', { remaining });
         }
     }
 }
