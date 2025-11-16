1b48c41d842fa9a84e4c6139595c3b05a8685d3a
Florent Morselli <florent.morselli@spomky-labs.com>
2025-11-16 20:38:11 +0100

    Add initial implementation of Web APIs Stimulus Controllers and CI for NPM publishing (#389)
    * Update package metadata and add initial index file for Stimulus controllers
    * Update repository URL format in package.json

---
+++ assets/src/index.js
@@ -0,0 +1,32 @@
+/**
+ * @spomky-labs/pwa-bundle
+ * Web APIs Stimulus Controllers
+ */
+
+export { default as BackgroundFetchController } from './background-fetch_controller.js';
+export { default as BackgroundSyncFetchController } from './backgroundsync-form_controller.js';
+export { default as BackgroundQueueController } from './backgroundsync-queue_controller.js';
+export { default as BadgeController } from './badge_controller.js';
+export { default as BarcodeDetectionController } from './barcode-detection_controller.js';
+export { default as BatteryController } from './battery_controller.js';
+export { default as CaptureController } from './capture_controller.js';
+export { default as ConnectionStatusController } from './connection-status_controller.js';
+export { default as ContactController } from './contact_controller.js';
+export { default as DeviceMotionController } from './device-motion_controller.js';
+export { default as DeviceOrientationController } from './device-orientation_controller.js';
+export { default as FileHandlingController } from './file-handling_controller.js';
+export { default as FullscreenController } from './fullscreen_controller.js';
+export { default as GeolocationController } from './geolocation_controller.js';
+export { default as InstallController } from './install_controller.js';
+export { default as NetworkInformationController } from './network-information_controller.js';
+export { default as PictureInPictureController } from './picture-in-picture_controller.js';
+export { default as PrefetchOnDemandController } from './prefetch-on-demand_controller.js';
+export { default as PresentationController } from './presentation_controller.js';
+export { default as ReceiverController } from './receiver_controller.js';
+export { default as ServiceWorkerController } from './service-worker_controller.js';
+export { default as SpeechSynthesisController } from './speech-synthesis_controller.js';
+export { default as TouchController } from './touch_controller.js';
+export { default as VibrationController } from './vibration_controller.js';
+export { default as WakeLockController } from './wake-lock_controller.js';
+export { default as WebPushController } from './web-push_controller.js';
+export { default as WebShareController } from './web-share_controller.js';
